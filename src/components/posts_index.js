d9cd6d9f3e08418efe3109320612d81e79d6b036
tophermurphy <tophermurphy@gmail.com>
2018-02-27 07:12:28 -0600

    "mornin toot"

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions/index';
 import _ from 'lodash';
+import {Link} from 'react-router-dom';
+
 
 class PostsIndex extends Component {
     componentDidMount(){
@@ -21,6 +23,11 @@ class PostsIndex extends Component {
     render(){
         return(
             <div>
+                <div className="test-xs-right">
+                    <Link className="btn btn-primary" to="/posts/new">
+                        Add a Posts
+                    </ Link>
+                </div>
                 <h3>Posts</h3>
                 <ul className="list-group">
                     {this.renderPosts()}
