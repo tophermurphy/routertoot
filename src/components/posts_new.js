86c129871edafcdfd337242bb8f4457fe3867a21
tophermurphy <tophermurphy@gmail.com>
2018-02-28 08:12:59 -0600

    "morning toot"

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -2,12 +2,37 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+    renderField(field){
+        return(
+            <div className="form-group">
+                <label>{field.label}</label>
+                <input
+                    className="form-control"
+                    type="text"
+                    {...field.input}
+                />
+            </div>
+        );
+    }
+
+
     render(){
         return (
             <form>
                 <Field
+                    label="Title for Post"
                     name="title"
-                    component={}
+                    component={this.renderField}
+                />
+                <Field
+                    label="Tags"
+                    name="tags"
+                    component={this.renderField}
+                />
+                <Field
+                    label="Post Content"
+                    name="content"
+                    component={this.renderField}
                 />
             </form>
         );
