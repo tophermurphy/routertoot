90e5fc2479a827a4a7e2db45a412b6389032147a
cmurphy <cmurphy@glunzbeers.com>
2018-02-28 13:43:57 -0600

    'work toot'

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -11,6 +11,7 @@ class PostsNew extends Component {
                     type="text"
                     {...field.input}
                 />
+                {field.meta.error}
             </div>
         );
     }
@@ -25,8 +26,8 @@ class PostsNew extends Component {
                     component={this.renderField}
                 />
                 <Field
-                    label="Tags"
-                    name="tags"
+                    label="Categories"
+                    name="categories"
                     component={this.renderField}
                 />
                 <Field
@@ -39,6 +40,27 @@ class PostsNew extends Component {
     }
 }
 
+function validate(values){
+    const errors = {};
+
+    //Validate the inputs from 'values'
+    if(!values.title){
+        errors.title = "Enter a Title!";
+    }
+    if(!values.categories){
+        errors.categories = "Enter some categories";
+    }
+    if(!values.content){
+        errors.content = "Enter some content please";
+    }
+
+    //if errors is empty, the form is fine to subumit
+    //if errors has *any* properties, redux form assumes form is invalid
+    return errors;
+
+}
+
 export default reduxForm({
+    validate,
     form: 'PostsNewForm'
 })(PostsNew);
