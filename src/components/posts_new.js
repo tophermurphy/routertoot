28ed7a1dbb07b30d288999a649392d037844c183
cmurphy <cmurphy@glunzbeers.com>
2018-03-02 13:35:54 -0600

    "work toot"

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -25,7 +25,10 @@ class PostsNew extends Component {
     }
 
     onSubmit(values){
-        this.props.createPost(values);
+        
+        this.props.createPost(values, () => {
+            this.props.history.push('/');
+        } );
     }
 
     render(){
